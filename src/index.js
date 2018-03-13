358b92fef6fbcdb4f938751fdca77a3b5597972d
Meghan Shafer <shafer949@gmail.com>
2018-03-13 11:23:49 -0500

    chore: boilerplate setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import SocialTriviaApp from './components/SocialTriviaApp'
+
+render(<SocialTriviaApp/>, document.getElementById('root'))
