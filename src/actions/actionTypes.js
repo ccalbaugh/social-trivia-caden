284aae6e52195f5bab9fa0c18c13a3aca6505d73
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-13 12:55:06 -0500

    refactor: break answers actions and tests into their own files

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -2,3 +2,4 @@ export const SUBMIT_ANSWER = 'SUBMIT_ANSWER';
 export const START_TIMER = 'START_TIMER';
 export const RESET_TIMER = 'RESET_TIMER';
 export const UPDATE_TIMER = 'UPDATE_TIMER';
+export const DECREMENT_TIMER = 'DECREMENT_TIMER';
