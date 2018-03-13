bb9c2752deebac22567dfd24cc007906a030b1ec
Meghan Shafer <shafer949@gmail.com>
2018-03-13 11:30:42 -0500

    feat: initial setup of teams

---
+++ src/components/Teams/teams.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Teams extends Component {
+
+    render() {
+        return (
+            <section>
+                Placeholder
+            </section>
+        )
+    }
+}
+
+export default Teams
