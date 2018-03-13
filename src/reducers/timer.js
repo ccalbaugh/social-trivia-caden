50ae8d07b7827208dec4e75d5d2d10c82c9b9cb4
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-13 16:21:48 -0500

    feat: create startTimer per corresponding test spec

--- src/reducers/timer.js
+++ src/reducers/timer.js
@@ -1,7 +1,16 @@
 import * as types from '../actions/actionTypes';
 
+function startTimer(state, action) {
+    return {
+        ...state,
+        isTimerRunning: action.isTimerRunning
+    };
+}
+
 export default function(state = {}, action) {
-    const actionsHandler = {};
+    const actionsHandler = {
+        [types.START_TIMER]: startTimer
+    };
 
     const reducer = actionsHandler[action.type];
 
