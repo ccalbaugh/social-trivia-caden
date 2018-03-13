dbd4a2177ef71605dcab6af0301d9c1b5999838c
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-13 12:09:32 -0500

    test: ensure there is an action to submit an answer

---
+++ src/actions/actions.test.js
@@ -0,0 +1,15 @@
+import 'isomorphic-fetch';
+import { expect } from 'code';
+import sinon from 'sinon';
+import * as actions from './actions';
+import * as types from './actionTypes';
+
+it('creates an action to submit an answer', () => {
+
+    const mockAnswer = 10;
+
+    const expectedAction = { type: types.SUBMIT_ANSWER, answer: mockAnswer };
+
+    expect(actions.submitAnswer(mockAnswer)).to.equal(expectedAction);
+
+});
