7194b3d6cd970acf9083d1867cb630d3c0ba6b44
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-13 14:59:58 -0500

    test: ensure timerReducer returns state if nothing is given

---
+++ src/reducers/timer.test.js
@@ -0,0 +1,17 @@
+import { expect } from 'code';
+import * as types from '../actions/actionTypes';
+import timerReducer from './timer';
+
+
+
+describe('Given `timerReducer`', () => {
+
+    it('should return the initial state when none is given', () => {
+
+        const expectedState = {};
+
+        expect(timerReducer(undefined, { type: 'NO_MATCH'})).to.equal(expectedState);
+
+    });
+
+});
