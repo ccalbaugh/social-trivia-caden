395a4d28b21e5761f4ec94a0446648f14f59eb00
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-14 11:24:37 -0500

    feat: create buttons in Timer per corresponding test

--- src/components/Timer/timer.js
+++ src/components/Timer/timer.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react'
 class Timer extends Component {
 
     render() {
+        const { currentTime } = this.props
         return (
             <section>
-                Placeholder
+                <span className="current-time">{currentTime}</span>
+                <button className="start-timer-button">Start</button>
+                <button className="pause-timer-button">Start</button>
+                <button className="reset-timer-button">Start</button>                                                
             </section>
         )
     }
