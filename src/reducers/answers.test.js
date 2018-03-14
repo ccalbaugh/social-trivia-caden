3e2841ae6bb56cbe5f4c356e8c617fcb6ba09de3
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-14 10:27:04 -0500

    test: update test to better reflect expectations

--- src/reducers/answers.test.js
+++ src/reducers/answers.test.js
@@ -3,7 +3,7 @@ import * as types from '../actions/actionTypes';
 import answersReducer from './answers';
 
 const mockAnswer = 10;
-const mockId = 1;
+const mockId = 12345;
 
 describe('Given `answersReducer`', () => {
 
@@ -17,7 +17,7 @@ describe('Given `answersReducer`', () => {
 
     it('should handle SUBMIT_ANSWER', () => {
 
-        const expectedState = {  1:  10  };
+        const expectedState = {  [mockId]:  mockAnswer  };
 
         expect(answersReducer(undefined, { type: types.SUBMIT_ANSWER, answer: mockAnswer, id: mockId })).to.equal(expectedState);
 
