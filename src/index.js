f5ecba38d3cd45e8a9202a7a361f8a83e15300ab
Meghan Shafer <shafer949@gmail.com>
2018-03-14 13:43:38 -0500

    fix: missing imports in index.js from last PR merge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import SocialTriviaApp from './components/SocialTriviaApp';
 import answers from './reducers/answers';
