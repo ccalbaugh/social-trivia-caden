fd7482ba376fc17e82a74a7a68e918bd0a18e064
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-14 12:10:48 -0500

    feat: create logic to dispatch action on start button click per test spec

--- src/components/Timer/timer.js
+++ src/components/Timer/timer.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { 
+    controlTimer, 
+    resetTimer, 
+    updateTimer, 
+    decrementTimer 
+} from '../../actions/timer'
 
-class Timer extends Component {
+function controlTimerClick(isTimerRunning) {
+    controlTimer(isTimerRunning);
+}
+
+export class Timer extends Component {
 
     render() {
-        const { currentTime, isTimerRunning } = this.props
+        const { currentTime, isTimerRunning, defaultTime } = this.props
         return (
             <section>
                 <span className="current-time">{currentTime}</span>
-                <button className="start-timer-button" disabled={isTimerRunning}>Start</button>
-                <button className="pause-timer-button" disabled={!isTimerRunning}>Start</button>
-                <button className="reset-timer-button">Start</button>                                                
+                <button className="start-timer-button" 
+                        disabled={isTimerRunning}
+                        onClick={controlTimerClick.bind(this, isTimerRunning)}
+                >Start</button>
+                <button className="pause-timer-button" disabled={!isTimerRunning}>Pause</button>
+                <button className="reset-timer-button">Reset</button>                                                
             </section>
         )
     }
 }
 
-export default Timer
+function mapStateToProps(state) {
+    return {
+        currentTime: state.currentTime,
+        isTimerRunning: state.isTimerRunning,
+        defaultTime: state.defaultTime
+    };
+}
+
+export default connect(mapStateToProps, { controlTimer, resetTimer, updateTimer, decrementTimer })(Timer)
