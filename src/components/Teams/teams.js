c8c9f06336887c3cbc565ce801110fe2da249b89
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-15 13:55:52 -0500

    feat: create logic to fulfill test spec

--- src/components/Teams/teams.js
+++ src/components/Teams/teams.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import AnswerForm from '../Form/answerForm'
 
 class Teams extends Component {
+
+    state = {
+        teams: [
+            { id: 'team-1' },
+            { id: 'team-2' },
+            { id: 'team-3' },
+            { id: 'team-4' },
+            { id: 'team-5' },
+            { id: 'team-6' } 
+        ]
+    }
 
     render() {
         return (
             <section>
-                <ul className="team-list"></ul>
+                <ul className="team-list">
+                    {
+                        this.state.teams.map((team) => {
+                            return <li className="team-list-item" key={team.id}><AnswerForm id={team.id} /></li>
+                        })
+                    }
+                </ul>
             </section>
         )
     }
