65ace26d4336ad14274262e99793396005fe9668
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-16 16:17:07 -0500

    feat: rename file and create update logic to fulfill test spec

--- src/reducers/teams.js
+++ src/reducers/teams.js
@@ -1,4 +1,5 @@
 import * as types from '../actions/actionTypes';
+import { combineReducers } from 'redux'
 
 function submitAnswer(state, action) {
     return action.id ? {
@@ -11,9 +12,21 @@ function submitAnswer(state, action) {
     state
 }
 
+function updateTeam(state, action) {
+    return {
+        ...state,
+        [action.id]: {
+            answer: null,
+            timeStamp: null,
+            score: action.score,
+        }
+    };
+}
+
 export default function(state = {}, action) {
     const actionsHandler = {
-        [types.SUBMIT_ANSWER]: submitAnswer
+        [types.SUBMIT_ANSWER]: submitAnswer,
+        [types.UPDATE_TEAM]: updateTeam
     };
 
     const reducer = actionsHandler[action.type];
