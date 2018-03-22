57b71cd054b8e6a2efbd44342f59035b2c0445c1
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-22 14:33:04 -0500

    feat: update logic to reflect tests and expected state behavior

--- src/actions/timer.js
+++ src/actions/timer.js
@@ -1,29 +1,69 @@
 import * as types from './actionTypes';
+import { database } from '../data/firebase'
+
+const timer = database.ref('timer/')
+
+export function createTimer() {
+    return dispatch => {
+        timer.set({ isTimerRunning: false, currentTime: 60, defaultTime: 60 })
+        dispatch({
+            type: types.CREATE_TIMER,
+            isTimerRunning: false
+        })
+    };
+}
 
 export function updateTimer(setTime) {
-    return {
-        type: types.UPDATE_TIMER,
-        setTime
+    return dispatch => {
+        timer.update({ 
+            isTimerRunning: false,
+            currentTime: setTime,
+            defaultTime: setTime
+        })
+        dispatch({
+            type: types.UPDATE_TIMER,
+            setTime
+        })
     };
 } 
 
 export function controlTimer(isTimerRunning) {
-    return {
-        type: types.CONTROL_TIMER,
-        isTimerRunning
+    return dispatch => {
+        timer.update({ isTimerRunning })
+        dispatch({
+            type: types.CONTROL_TIMER,
+            isTimerRunning
+        })
     };
 }
 
 export function resetTimer(defaultTime) {
-    return {
-        type: types.RESET_TIMER,
-        defaultTime
+    return dispatch => {
+        timer.update({ isTimerRunning: false, currentTime: defaultTime })
+        dispatch({
+            type: types.RESET_TIMER,
+            currentTime: defaultTime
+        })
     };
 } 
 
 export function decrementTimer(timeLeft) {
-    return {
-        type: types.DECREMENT_TIMER,
-        timeLeft
+    return dispatch => {
+        timer.update({ currentTime: timeLeft - 1 })
+        dispatch({
+            type: types.DECREMENT_TIMER,
+            timeLeft
+        }) 
     };
 } 
+
+export function fetchTimer() {
+    return dispatch => {
+        timer.on('value', snapshot => {
+          dispatch({
+              type: types.FETCH_TIMER,
+              timer: snapshot.val()
+          })
+        });
+      }
+}
